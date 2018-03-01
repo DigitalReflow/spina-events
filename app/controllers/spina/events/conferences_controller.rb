46f7a00f9e3d607021458ef210f237bfd1575618
Dan Mitchell <dan@digitalreflow.co.uk>
2018-03-01 15:27:49 +0000

    Remove root path

--- app/controllers/spina/events/conferences_controller.rb
+++ app/controllers/spina/events/conferences_controller.rb
@@ -2,18 +2,13 @@ module Spina::Events
   class ConferencesController < ApplicationController
 
     before_action :set_page
-    before_action :find_conferences, only: [:index]
 
     def index
-      respond_to do |format|
-        format.atom
-        format.html { render layout: "#{current_theme.name.parameterize.underscore}/application" }
-      end
+      @conferences = Spina::Events::Conference.live.order(title: :asc)
     end
 
     def show
       @conference = Spina::Events::Conference.friendly.find(params[:id])
-      render layout: "#{current_theme.name.parameterize.underscore}/application"
     rescue ActiveRecord::RecordNotFound
       try_redirect
     end
@@ -21,19 +16,15 @@ module Spina::Events
     private
 
     def set_page
-      @page = Spina::Page.find_or_create_by(name: 'events') do |page|
-        page.title = 'Events'
-        page.link_url = '/events'
+      @page = Spina::Page.find_or_create_by(name: 'conferences') do |page|
+        page.title = 'Conferences'
+        page.link_url = '/conferences'
         page.deletable = false
       end
     end
 
-    def find_conferences
-      @conferences = Spina::Events::Conference.live.order(title: :asc).page(params[:page])
-    end
-
     def try_redirect
-      rule = RewriteRule.find_by!(old_path: "/events/conference/#{params[:id]}")
+      rule = RewriteRule.find_by!(old_path: "/conferences/#{params[:id]}")
       redirect_to rule.new_path, status: :moved_permanently
     end
   end
