3ec683c358bf53946ecebf4bbb909c01909e2352
Matthew Witek <matt@mwitekdesign.com>
2018-03-29 20:37:48 -0400

    Add session table

---
+++ db/migrate/20180330002910_create_spina_events_sessions.rb
@@ -0,0 +1,12 @@
+class CreateSpinaEventsSessions < ActiveRecord::Migration[5.1]
+  def change
+    create_table :spina_events_sessions do |t|
+      t.integer :conference_id
+      t.string :title
+      t.text :description
+      t.datetime :start_at
+      t.datetime :end_at
+    end
+    add_index :spina_events_sessions, :conference_id
+  end
+end
