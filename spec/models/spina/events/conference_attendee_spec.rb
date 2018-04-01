06dfeb8aff29786cbcea8c69a2008369600c2eb0
Dan Mitchell <dan@digitalreflow.co.uk>
2018-04-01 23:33:31 +0100

    Add shoulda-matchers and refactor validation specs
    Please enter the commit message for your changes. Lines starting

--- spec/models/spina/events/conference_attendee_spec.rb
+++ spec/models/spina/events/conference_attendee_spec.rb
@@ -2,16 +2,7 @@ require 'rails_helper'
 
 module Spina::Events
   RSpec.describe ConferenceAttendee, type: :model do
-    it 'validates presence of conference' do
-      conference_attendee = ConferenceAttendee.new
-      expect(conference_attendee.valid?).to be(false)
-      expect(conference_attendee.errors.keys).to include(:conference_id)
-    end
-
-    it 'validates presence of organisation' do
-      conference_attendee = ConferenceAttendee.new
-      expect(conference_attendee.valid?).to be(false)
-      expect(conference_attendee.errors.keys).to include(:organisation_id)
-    end
+    it { should validate_presence_of(:conference_id) }
+    it { should validate_presence_of(:organisation_id) }
   end
 end
--- spec/models/spina/events/session_type_spec.rb
+++ spec/models/spina/events/session_type_spec.rb
@@ -2,10 +2,6 @@ require 'rails_helper'
 
 module Spina::Events
   RSpec.describe SessionType, type: :model do
-    it 'validates presence of name' do
-      session_type = SessionType.new
-      expect(session_type.valid?).to be(false)
-      expect(session_type.errors.keys).to include(:name)
-    end
+    it { should validate_presence_of(:name) }
   end
 end
