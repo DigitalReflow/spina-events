2512b9833e21fa7cf62f14bc7bd6d6e014652b4c
Matthew Witek <matt@mwitekdesign.com>
2018-03-31 21:25:14 -0400

    Add spec coverage for conference_speaker and conference_attendee

--- app/models/spina/events/conference_speaker.rb
+++ app/models/spina/events/conference_speaker.rb
@@ -1,5 +1,6 @@
 module Spina::Events
   class ConferenceSpeaker < ApplicationRecord
+    validates :conference_id, :speaker_id, presence: true
     belongs_to :conference
     belongs_to :speaker
   end
