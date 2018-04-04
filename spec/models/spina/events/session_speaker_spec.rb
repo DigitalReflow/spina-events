387198d912ea10ba5c2d8bfa2c9babe7453dc20c
Matthew Witek <matt@mwitekdesign.com>
2018-04-04 00:03:07 -0400

    Add session_speakers to form

--- spec/models/spina/events/session_speaker_spec.rb
+++ spec/models/spina/events/session_speaker_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 module Spina::Events
   RSpec.describe SessionSpeaker, type: :model do
-    it { should validate_presence_of(:session_id) }
-    it { should validate_presence_of(:speaker_id) }
+    it { should validate_presence_of(:session) }
+    it { should validate_presence_of(:speaker) }
   end
 end
