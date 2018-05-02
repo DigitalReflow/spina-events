eb3b663f0ce7d308a7cdb24112620fb5f1816755
Matthew Witek <matt@mwitekdesign.com>
2018-05-01 21:13:54 -0400

    Update sorting so that it works

--- app/controllers/spina/events/admin/conference/speakers_controller.rb
+++ app/controllers/spina/events/admin/conference/speakers_controller.rb
@@ -8,7 +8,7 @@ module Spina::Events
         layout 'spina/admin/events'
 
         def index
-          @speakers = current_conference.speakers.order(created_at: :desc)
+          @speakers = current_conference.speakers
         end
 
         def edit
@@ -33,20 +33,20 @@ module Spina::Events
         end
 
         def sort
-          params[:list].each_with_index do |speaker, position|
-            update_speaker_position(speaker, position)
+          params[:list].each do |position, speaker|
+            update_speaker_position(speaker[:id], position)
           end
           head :ok
         end
 
-        def update_speaker_position(speaker, position)
-          Spina::Events::ConferenceSpeaker.update(speaker_id: speaker[:id], position: position.to_i + 1, conference_id: current_conference[:id])
+        def update_speaker_position(speaker_id, position)
+          current_conference.conference_speakers.where(speaker_id: speaker_id).update(position: position.to_i + 1)
         end
 
         private
 
         def current_conference
-          @conference ||= Spina::Events::Conference.find(params[:conference_id])
+          @conference ||= Spina::Events::Conference.includes(:conference_speakers).find(params[:conference_id])
         end
 
         def set_breadcrumb
